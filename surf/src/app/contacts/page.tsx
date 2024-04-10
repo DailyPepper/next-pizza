52e22010354064038bcd76a63b8f565a26423fe8
Daniel Kudinov <danielkudinov17@gmail.com>
2024-04-10 16:06:45 +0300

    start

---
+++ surf/src/app/contacts/page.tsx
@@ -0,0 +1,11 @@
+const contact = () => {
+    return ( 
+        <>
+            <h1>
+                contact
+            </h1>
+        </>
+     );
+}
+ 
+export default contact;
---
+++ surf/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { notFound } from "next/navigation";
+
+const NotFound = () => {
+    return ( 
+        <h1>
+            Такого адреса нету
+        </h1>
+     );
+}
+ 
+export default NotFound;
